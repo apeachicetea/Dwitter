aa1b043fe7b83a9bb287c4293a5a047e7c372815
kimdaeyoon <eodbszla@gmail.com>
2021-12-29 14:46:24 +0900

    MVC-controller

--- server/router/tweets.js
+++ server/router/tweets.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import 'express-async-errors';
-import * as tweetRepository from '../data/tweet.js';
+import * as tweetController from '../controller/tweet.js';
 //모든 함수들을 tweetRepository의 이름으로 불러온다.
 const router = express.Router();
 
@@ -8,51 +8,18 @@ const router = express.Router();
 
 // GET /tweets
 // GET /tweets?username=:username
-router.get('/', (req, res, next) => {
-  const username = req.query.username;
-  const data = username
-    ? tweetRepository.getAllByUsername(username);
-    : tweetRepository.getAll();
-  res.status(200).json(data);
-});
+router.get('/', tweetController.getTweets);
 
 // GET /tweets/:id
-router.get('/:id', (req, res, next) => {
-  const id = req.params.id;
-  const tweet = tweetRepository.getAllById(id);
-  if(tweet) {
-    res.status(200).json(tweet);
-  }
-  else {
-    res.status(404).json({ message: `Tweet id(${id}) not found` });
-  }
-});
+router.get('/:id', tweetController.getTweet);
 
 // POST /tweeets
-router.post('/', (req, res, next) => {
-  const { text, username, name } = req.body;
-  const tweet = tweetRepository.create(text, username, name);
-  res.status(201).json(tweet);
-});
+router.post('/', tweetController.createTweet);
 
 // PUT /tweets/:id
-router.put('/:id', (req, res, next) => {
-  const id = req.params.id;
-  const text = req.body.text;
-  const tweet = tweetRepository.update(id, text);
-  if(tweet) {
-    res.status(200).json(tweet);
-  }
-  else {
-    res.status(404).json({ message: `Tweet id(${id}) not found` })
-  }
-});
+router.put('/:id', tweetController.updateTweet);
 
 // DELETE /tweets/:id
-router.delete('/:id', (req, res, next) => {
-  const id = req.params.id;
-  tweetRepository.remove(id);
-  res.sendStatus(204);
-});
+router.delete('/:id', tweetController.deleteTweet);
 
 export default router;
